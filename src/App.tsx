eae66a5847021c34f5e95de61615741b8d9a5f72
Unicornelia <ez.kornelia@gmail.com>
2025-03-29 15:34:33 +0100

    fix: update gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import styled from 'styled-components';
-import './App.css';
 import { ErrorBoundary } from 'react-error-boundary';
 import { IStyledComponentBase } from 'styled-components/dist/types';
 import { Goal } from './components/Goals/types.ts';
 import { initialGoals } from './components/Goals/data.ts';
-import Message from './components/Message.tsx';
 import NewGoal from './components/Goals/NewGoal.tsx';
 import GoalList from './components/Goals/GoalList.tsx';
-import UserContext from './context/UserContext.tsx';
+import './App.css';
 
 const Container: IStyledComponentBase<'web'> = styled.div`
   display: flex;
@@ -28,34 +26,19 @@ type GoalHandlerFn = (goal: Goal) => void;
 
 const App: FC = () => {
   const [courseGoals, setCourseGoals] = useState(initialGoals);
-  const [userName, setUserName] = useState<string>('User');
-  const [message, setMessage] = useState<string>('Message');
 
   const addNewGoalHandler: GoalHandlerFn = (goal: Goal): void => {
     setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(goal));
   };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      // update state after 5 sec
-      setUserName('Kori');
-      setMessage('Goodbye');
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   let date = new Date().toDateString();
   return (
     <ErrorBoundary fallback={<p>Something went wrong</p>}>
-      <UserContext.Provider value={{ userName: userName, message: message }}>
-        <Container>
-          <h2>Todo for {date}</h2>
-          <NewGoal onAddGoal={addNewGoalHandler} />
-          <GoalList goals={courseGoals} />
-          <Message />
-        </Container>
-      </UserContext.Provider>
+      <Container>
+        <h2>Todo for {date}</h2>
+        <NewGoal onAddGoal={addNewGoalHandler} />
+        <GoalList goals={courseGoals} />
+      </Container>
     </ErrorBoundary>
   );
 };
--- src/components/Goals/NewGoal.tsx
+++ src/components/Goals/NewGoal.tsx
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 import AnimatedButton from './AnimatedButton';
 
 const StyledForm = styled.form`
-    text-align: center;
-    margin-bottom: 3vh;
-    display: flex;
-    flex-direction: column;
+  text-align: center;
+  margin-bottom: 3vh;
+  display: flex;
+  flex-direction: column;
 
-    input {
-        padding: 1vh;
-        border-radius: 6px;
-        border: 1px solid aquamarine;
-    }
+  input {
+    padding: 1vh;
+    border-radius: 6px;
+    border: 1px solid aquamarine;
+  }
 `;
 
 type onAddGoalType = { onAddGoal: (newGoal: { id: number; text: string }) => void };
@@ -25,7 +25,7 @@ const NewGoal = ({ onAddGoal }: onAddGoalType) => {
 
     const newGoal = {
       id: Math.random(),
-      text: inputText
+      text: inputText,
     };
     setInputText('');
     onAddGoal(newGoal);
@@ -38,7 +38,9 @@ const NewGoal = ({ onAddGoal }: onAddGoalType) => {
   return (
     <StyledForm className="new-goal" onSubmit={addGoalHandler}>
       <input type="text" onChange={textChangeHandler} value={inputText} />
-      <AnimatedButton type="submit" value="Add Goal">Add Goal</AnimatedButton>
+      <AnimatedButton type="submit" value="Add Goal">
+        Add Goal
+      </AnimatedButton>
     </StyledForm>
   );
 };
