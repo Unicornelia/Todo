79939cabc768e40b931eda1c0b3ba5c68c2d4558
Unicornelia <ez.kornelia@gmail.com>
2025-04-05 13:09:47 +0200

    feat: simple forms and a controlled modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
 import FallBackError from './components/FallBackError.tsx';
 import { IStyledComponentBase } from 'styled-components/dist/types';
 import styled from 'styled-components';
+import { ControlledModal } from './components/Modal/ControlledModal.tsx';
 
 const Container: IStyledComponentBase<'web'> = styled.div`
   display: flex;
@@ -20,9 +21,17 @@ const Container: IStyledComponentBase<'web'> = styled.div`
 `;
 
 const App: FC<Element> = () => {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
+
+  let toggleModal = () => setShouldShowModal((prevState) => !prevState);
   return (
     <ErrorBoundary fallback={<FallBackError />}>
-      <Container></Container>
+      <Container>
+        <ControlledModal shouldShow={shouldShowModal} onToggle={toggleModal}>
+          <h1>Hi there</h1>
+        </ControlledModal>
+        <button onClick={toggleModal}>{shouldShowModal ? 'Hide' : 'Reveal'}</button>
+      </Container>
     </ErrorBoundary>
   );
 };
